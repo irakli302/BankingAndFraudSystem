5e2291e7c5fdf3972115300b5b9efc2782437545
Irakli Dzandzava <iraklidzandzava5@gmail.com>
2026-02-21 01:21:29 +0400

    IMPLEMENTED Withdrawal class.

--- src/bankingandfraudsystem/domain/transaction/Withdrawal.java
+++ src/bankingandfraudsystem/domain/transaction/Withdrawal.java
@@ -1,4 +1,32 @@
 package bankingandfraudsystem.domain.transaction;
 
+import bankingandfraudsystem.Exception.CurrencyMismatchException;
+import bankingandfraudsystem.domain.account.Account;
+import bankingandfraudsystem.util.Money;
+
 public class Withdrawal extends Transaction{
+    private final Account from;
+
+    public Withdrawal(Money amount, String description, Account fromAcc) {
+        super(amount,description);
+
+        if(fromAcc==null) throw new IllegalArgumentException("Target account cannot be null!");
+        if(!fromAcc.getCurrency().equals(amount.getCurrency())) throw new IllegalStateException("Currency mismatch!");
+
+        this.from = fromAcc;
+    }
+
+    public Account getFromAccount() {
+        return this.from;
+    }
+
+    @Override
+    public TransactionType type() {
+        return TransactionType.WITHDRAWAL;
+    }
+
+    @Override
+    protected void apply() throws CurrencyMismatchException {
+        from.withDraw(getAmount());
+    }
 }
