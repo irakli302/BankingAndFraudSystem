ad888a43be5c656c2a2a4c308313979ab38c2ffb
Irakli Dzandzava <iraklidzandzava5@gmail.com>
2026-02-24 15:28:29 +0400

    IMPLEMENTED TooManyTransactionsRule class.

--- src/bankingandfraudsystem/rules/impl/TooManyTransactionsRule.java
+++ src/bankingandfraudsystem/rules/impl/TooManyTransactionsRule.java
@@ -1,4 +1,50 @@
 package bankingandfraudsystem.rules.impl;
 
-public class TooManyTransactionsRule {
+import bankingandfraudsystem.domain.transaction.Transaction;
+import bankingandfraudsystem.rules.Decision;
+import bankingandfraudsystem.rules.FraudContext;
+import bankingandfraudsystem.rules.FraudRule;
+import bankingandfraudsystem.rules.RuleResult;
+
+public class TooManyTransactionsRule implements FraudRule {
+    private final int maxCount;
+    private final int windowMinutes;
+    private final Decision decisionOnHit;
+
+    public TooManyTransactionsRule(int maxCnt, int windowMnts, Decision decision) {
+        if(maxCnt < 1) throw new IllegalArgumentException("MaxCount should be greater then 0!");
+
+        if(windowMnts < 1) throw new IllegalArgumentException("WindowMinutes should be greater then 0!");
+
+        if(decision == null) throw new IllegalArgumentException("Decision cannot be null!");
+
+        if(decision == Decision.ALLOW) throw new IllegalArgumentException("Decision should not be ALLOW!");
+
+        this.maxCount = maxCnt;
+        this.windowMinutes = windowMnts;
+        this.decisionOnHit = decision;
+    }
+
+    @Override
+    public RuleResult evaluate(Transaction transaction, FraudContext ctx) {
+        if(transaction == null) throw new IllegalArgumentException("Transaction cannot be null!");
+        if(ctx == null) throw new IllegalArgumentException("FraudContext cannot be null!");
+
+        int count = ctx.countWithInMinutes(windowMinutes) + 1;
+        String reason = "High velocity: " + count + " transaction in " + this.windowMinutes + " minutes";
+
+        if(count > maxCount) {
+            return (this.decisionOnHit == Decision.BLOCK) ? RuleResult.block(reason) : RuleResult.review(reason);
+        }
+
+        return RuleResult.allow();
+    }
+
+    public int getMaxCount() {
+        return this.maxCount;
+    }
+
+    public int getWindowMinutes() {
+        return this.windowMinutes;
+    }
 }
