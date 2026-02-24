fbadba61e97ec65ca2f6ed959b406f678adfb08b
Irakli Dzandzava <iraklidzandzava5@gmail.com>
2026-02-24 16:06:11 +0400

    IMPLEMENTED VelocitySumRule class.

--- src/bankingandfraudsystem/rules/FraudRule.java
+++ src/bankingandfraudsystem/rules/FraudRule.java
@@ -1,7 +1,8 @@
 package bankingandfraudsystem.rules;
 
+import bankingandfraudsystem.Exception.CurrencyMismatchException;
 import bankingandfraudsystem.domain.transaction.Transaction;
 
 public interface FraudRule {
-    RuleResult evaluate(Transaction transaction, FraudContext ctx);
+    RuleResult evaluate(Transaction transaction, FraudContext ctx) throws CurrencyMismatchException;
 }
--- src/bankingandfraudsystem/rules/impl/VelocitySumRule.java
+++ src/bankingandfraudsystem/rules/impl/VelocitySumRule.java
@@ -1,9 +1,15 @@
 package bankingandfraudsystem.rules.impl;
 
+import bankingandfraudsystem.Exception.CurrencyMismatchException;
+import bankingandfraudsystem.domain.transaction.Transaction;
 import bankingandfraudsystem.rules.Decision;
+import bankingandfraudsystem.rules.FraudContext;
+import bankingandfraudsystem.rules.FraudRule;
+import bankingandfraudsystem.rules.RuleResult;
+import bankingandfraudsystem.util.Currency;
 import bankingandfraudsystem.util.Money;
 
-public class VelocitySumRule {
+public class VelocitySumRule implements FraudRule {
     private final Money threshold;
     private final int windowMinutes;
 
@@ -16,5 +22,25 @@ public class VelocitySumRule {
 
         this.threshold = money;
         this.windowMinutes = windowMnts;
+    }
+
+    @Override
+    public RuleResult evaluate(Transaction transaction, FraudContext ctx) throws CurrencyMismatchException {
+        if(transaction == null) throw new IllegalArgumentException("Transaction cannot be null!");
+        if(ctx == null) throw new IllegalArgumentException("FraudContext cannot be null!");
+
+        if(transaction.getAmount().getCurrency() != this.threshold.getCurrency())
+            return RuleResult.allow();
+
+        Currency currency = transaction.getAmount().getCurrency();
+
+        Money sum = ctx.sumWithinMinutes(windowMinutes,currency);
+
+        Money projected = sum.add_Money(transaction.getAmount());
+
+        if(projected.compareTo(this.threshold) > 0)
+            return RuleResult.review("High spend: " + projected + " in last " + this.windowMinutes + " minutes!");
+
+        return RuleResult.allow();
     }
 }
