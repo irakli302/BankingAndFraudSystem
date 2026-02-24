89b6d39fd37929a5acfece7289056c7444900baf
Irakli Dzandzava <iraklidzandzava5@gmail.com>
2026-02-24 10:31:01 +0400

    Updated FraudRule Interface.

--- src/bankingandfraudsystem/rules/FraudRule.java
+++ src/bankingandfraudsystem/rules/FraudRule.java
@@ -1,5 +1,7 @@
 package bankingandfraudsystem.rules;
 
+import bankingandfraudsystem.domain.transaction.Transaction;
+
 public interface FraudRule {
-    
+    RuleResult evaluate(Transaction transaction, FraudContext ctx);
 }
