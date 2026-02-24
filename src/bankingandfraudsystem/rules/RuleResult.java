16d214e59a9112da40adea39fadc923c72bc6938
Irakli Dzandzava <iraklidzandzava5@gmail.com>
2026-02-24 10:21:26 +0400

    IMPLEMENTED RuleResult class.

--- src/bankingandfraudsystem/rules/RuleResult.java
+++ src/bankingandfraudsystem/rules/RuleResult.java
@@ -1,5 +1,37 @@
 package bankingandfraudsystem.rules;
 
 public class RuleResult {
-    
+    private final Decision decision;
+    private final String reason;
+
+    private RuleResult(Decision dec, String reas) {
+        if(dec == null) throw new IllegalArgumentException("Decision cannot be null!");
+
+        if(dec == Decision.ALLOW) this.reason = null;
+        else {
+            if(reas == null || reas.isEmpty()) throw new IllegalArgumentException("Reason cannot be empty!");
+            this.reason = reas;
+        }
+        this.decision = dec;
+    }
+
+    public static RuleResult allow() {
+        return new RuleResult(Decision.ALLOW, null);
+    }
+
+    public static RuleResult review(String reason) {
+        return new RuleResult(Decision.REVIEW, reason);
+    }
+
+    public static RuleResult block(String reason) {
+        return  new RuleResult(Decision.BLOCK, reason);
+    }
+
+    public Decision getDecision() {
+        return this.decision;
+    }
+
+    public String getReason() {
+        return this.reason;
+    }
 }
