bbbd64525c566c9c14eb20a1af099519be792645
Irakli Dzandzava <iraklidzandzava5@gmail.com>
2026-02-24 23:01:50 +0400

    Added Accounts and Customers functional

--- src/bankingandfraudsystem/service/BankService.java
+++ src/bankingandfraudsystem/service/BankService.java
@@ -1,4 +1,84 @@
 package bankingandfraudsystem.service;
 
+import bankingandfraudsystem.domain.account.Account;
+import bankingandfraudsystem.domain.account.AccountStatus;
+import bankingandfraudsystem.domain.account.CheckingAccount;
+import bankingandfraudsystem.domain.account.SavingsAccount;
+import bankingandfraudsystem.domain.customer.Customer;
+import bankingandfraudsystem.domain.ledger.Ledger;
+import bankingandfraudsystem.domain.transaction.Transaction;
+import bankingandfraudsystem.rules.FraudEngine;
+import bankingandfraudsystem.util.Money;
+import bankingandfraudsystem.util.Currency;
+
+import java.util.*;
+
 public class BankService {
+    private final Ledger ledger;
+    private final FraudEngine fraudEngine;
+
+    private final Map<UUID, Customer>customers = new HashMap<>();
+    private final Map<UUID, Account>accounts = new HashMap<>();
+
+    private final List<Transaction>attempts = new ArrayList<>();
+
+    public BankService(Ledger ledger, FraudEngine fraudEngine) {
+        if(ledger == null) throw new IllegalArgumentException("Ledger cannot be null!");
+        if(fraudEngine == null) throw new IllegalArgumentException("FraudEngine cannot be null!");
+
+        this.ledger = ledger;
+        this.fraudEngine = fraudEngine;
+    }
+
+    private Customer requireCustomer(UUID id) {
+        if(id == null) throw new IllegalArgumentException("Customer ID cannot be null!");
+        if(!customers.containsKey(id)) throw new IllegalArgumentException("Customer not found!");
+
+        return customers.get(id);
+    }
+
+    private Account requireAccount(UUID id) {
+        if(id == null) throw new IllegalArgumentException("Account ID cannot be null!");
+        if(!accounts.containsKey(id)) throw new IllegalArgumentException("Account not found!");
+
+        return accounts.get(id);
+    }
+
+    public Customer createCustomer(String fullName) {
+        Customer customer = new Customer(fullName);
+        customers.put(customer.getId(), customer);
+        return customer;
+    }
+
+    public Customer getCustomer(UUID id) {
+        return requireCustomer(id);
+    }
+
+    public List<Customer>listCustomers() {
+        return List.copyOf(customers.values());
+    }
+
+    public CheckingAccount openChecking(UUID customerID, Currency currency, Money overdraftLimit) {
+        Customer owner = requireCustomer(customerID);
+        CheckingAccount checkingAccount = new CheckingAccount(owner,currency, AccountStatus.ACTIVE, overdraftLimit);
+        accounts.put(checkingAccount.getID(), checkingAccount);
+        owner.addAccount(checkingAccount);
+        return checkingAccount;
+    }
+
+    public SavingsAccount openSavings(UUID customerID, Currency currency, Money minimumBalance, int monthlyLimit) {
+        Customer owner = requireCustomer(customerID);
+        SavingsAccount savingsAccount = new SavingsAccount(owner,currency,AccountStatus.ACTIVE,minimumBalance,monthlyLimit);
+        accounts.put(savingsAccount.getID(), savingsAccount);
+        owner.addAccount(savingsAccount);
+        return savingsAccount;
+    }
+
+    public Account getAccount(UUID id) {
+        return requireAccount(id);
+    }
+
+    public List<Account>listAccounts() {
+        return List.copyOf(accounts.values());
+    }
 }
