7186a0be062e06ef1a3d52a57720c04b3decd7eb
Irakli Dzandzava <iraklidzandzava5@gmail.com>
2026-02-24 15:56:44 +0400

    Added Cosntructor to VelocitySumRule class.

--- src/bankingandfraudsystem/rules/impl/VelocitySumRule.java
+++ src/bankingandfraudsystem/rules/impl/VelocitySumRule.java
@@ -1,4 +1,20 @@
 package bankingandfraudsystem.rules.impl;
 
+import bankingandfraudsystem.rules.Decision;
+import bankingandfraudsystem.util.Money;
+
 public class VelocitySumRule {
+    private final Money threshold;
+    private final int windowMinutes;
+
+    public VelocitySumRule(Money money, int windowMnts) {
+        if(money == null) throw new IllegalArgumentException("Money cannot be null!");
+
+        if(!money.isPositive()) throw new IllegalArgumentException("Money must be positive!");
+
+        if(windowMnts < 1) throw new IllegalArgumentException("WindowMinutes should be greater then 0!");
+
+        this.threshold = money;
+        this.windowMinutes = windowMnts;
+    }
 }
