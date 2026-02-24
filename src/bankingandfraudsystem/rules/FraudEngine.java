b298fb292f309d53ee7af7019aea869263fbece7
Irakli Dzandzava <iraklidzandzava5@gmail.com>
2026-02-24 12:54:02 +0400

    IMPLEMNTED FraudEngine class.

--- src/bankingandfraudsystem/rules/FraudEngine.java
+++ src/bankingandfraudsystem/rules/FraudEngine.java
@@ -1,4 +1,36 @@
 package bankingandfraudsystem.rules;
 
+import bankingandfraudsystem.domain.transaction.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FraudEngine {
+    private final List<FraudRule>rules;
+
+    public FraudEngine(List<FraudRule>fraudRules) {
+        if(fraudRules == null) throw new IllegalArgumentException("Rules cannot be null!");
+
+        if(fraudRules.contains(null)) throw new IllegalArgumentException("Rules list contains null!");
+
+        this.rules = List.copyOf(fraudRules);
+    }
+
+    public RuleResult assess(Transaction tx, FraudContext ctx) {
+        if(tx == null) throw new IllegalArgumentException("Transaction cannot be null!");
+
+        if(ctx == null) throw new IllegalArgumentException("Fraud Context cannot be null!");
+
+        RuleResult best = RuleResult.allow();
+
+        for(FraudRule rule : this.rules) {
+            RuleResult result = rule.evaluate(tx,ctx);
+
+            if(result.getDecision() == Decision.BLOCK) return result;
+
+            if(result.getDecision() == Decision.REVIEW && best.getDecision() == Decision.ALLOW) best = result;
+        }
+
+        return best;
+    }
 }
