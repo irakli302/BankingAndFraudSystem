1bbbea647391aec606bb45ff31955b516ed10ca8
Irakli Dzandzava <iraklidzandzava5@gmail.com>
2026-02-27 23:42:39 +0400

    IMPLEMTED NewMerchantRule class.

--- src/bankingandfraudsystem/rules/impl/NewMerchantRule.java
+++ src/bankingandfraudsystem/rules/impl/NewMerchantRule.java
@@ -1,7 +1,12 @@
 package bankingandfraudsystem.rules.impl;
 
+import bankingandfraudsystem.Exception.CurrencyMismatchException;
+import bankingandfraudsystem.domain.transaction.CardPayment;
+import bankingandfraudsystem.domain.transaction.Transaction;
 import bankingandfraudsystem.rules.Decision;
+import bankingandfraudsystem.rules.FraudContext;
 import bankingandfraudsystem.rules.FraudRule;
+import bankingandfraudsystem.rules.RuleResult;
 
 public class NewMerchantRule implements FraudRule {
     private Decision decisionOnHit;
@@ -13,5 +18,23 @@ public class NewMerchantRule implements FraudRule {
             throw new IllegalArgumentException("Decision cannot be null!");
 
         this.decisionOnHit = decision;
+    }
+
+
+    @Override
+    public RuleResult evaluate(Transaction tx, FraudContext ctx) throws CurrencyMismatchException {
+        if(tx == null)
+            throw new IllegalArgumentException("Transaction cannot be null!");
+        if(ctx == null)
+            throw new IllegalArgumentException("FraudContext cannot be null!");
+
+        if(!(tx instanceof CardPayment cp)) return RuleResult.allow();
+
+        if (!ctx.hasSeenMerchant(cp.getMerchant().getId())) {
+            String reason = "New Merchant: " + cp.getMerchant().getFullName();
+            RuleResult result = this.decisionOnHit == Decision.BLOCK ? RuleResult.block(reason) : RuleResult.review(reason);
+            return result;
+        }
+        return RuleResult.allow();
     }
 }
