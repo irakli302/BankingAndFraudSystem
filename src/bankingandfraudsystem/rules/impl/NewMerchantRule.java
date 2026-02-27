463646ad9d8cfb63c7d3fbbb38d2b178f29e40ba
Irakli Dzandzava <iraklidzandzava5@gmail.com>
2026-02-27 23:32:30 +0400

    Added constructor to NewMerchantRule class.

--- src/bankingandfraudsystem/rules/impl/NewMerchantRule.java
+++ src/bankingandfraudsystem/rules/impl/NewMerchantRule.java
@@ -1,4 +1,17 @@
 package bankingandfraudsystem.rules.impl;
 
-public class NewMerchantRule {
+import bankingandfraudsystem.rules.Decision;
+import bankingandfraudsystem.rules.FraudRule;
+
+public class NewMerchantRule implements FraudRule {
+    private Decision decisionOnHit;
+
+    public NewMerchantRule(Decision decision) {
+        if(decision == null)
+            throw new IllegalArgumentException("Decision cannot be null!");
+        if(decision == Decision.ALLOW)
+            throw new IllegalArgumentException("Decision cannot be null!");
+
+        this.decisionOnHit = decision;
+    }
 }
