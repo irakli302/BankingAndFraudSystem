90629cc4b28c0ca6f5b1dc61ddf8ce12ad8b20d2
Irakli Dzandzava <iraklidzandzava5@gmail.com>
2026-02-28 02:10:19 +0400

    Added constructor to RapidLocationChangeRule class.

--- src/bankingandfraudsystem/rules/impl/RapidLocationChangeRule.java
+++ src/bankingandfraudsystem/rules/impl/RapidLocationChangeRule.java
@@ -1,4 +1,24 @@
 package bankingandfraudsystem.rules.impl;
 
+import bankingandfraudsystem.rules.Decision;
+
+import java.time.Duration;
+
 public class RapidLocationChangeRule {
+    private Duration maxTimeBetween;
+    private Decision decisionOnHit;
+
+    public RapidLocationChangeRule(Duration maxTimeBetween, Decision decision) {
+        if(maxTimeBetween == null)
+            throw new IllegalArgumentException("Time cannot be null!");
+        if(!maxTimeBetween.isPositive())
+            throw new IllegalArgumentException("Time must be positive!");
+        if(decision == null)
+            throw new IllegalArgumentException("Decision cannot be null!");
+        if(decision == Decision.ALLOW)
+            throw new IllegalArgumentException("Decision cannot be ALLOW!");
+
+        this.maxTimeBetween = maxTimeBetween;
+        this.decisionOnHit = decision;
+    }
 }
